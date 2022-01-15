eab397b270d770996ab1fa18586710cf230b981e
dimassdoubles <codedimas@gmail.com>
2022-01-15 21:12:50 +0700

    berhasil memilih image

---
+++ app/src/main/java/com/android/dimassdoubles/sambongmarket/BayarActivity.java
@@ -0,0 +1,14 @@
+package com.android.dimassdoubles.sambongmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BayarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bayar);
+    }
+}
